2e78345bc57ced9515855d47c642334e51b14ef9
chodge <hodgkinson@gmail.com>
2012-07-19 22:37:55 -0400

    add 'phone not handy' option

--- Site/Content/js/tour.js
+++ Site/Content/js/tour.js
@@ -36,10 +36,10 @@
 
                             if(status === 'inProgress') {
                                 
-                                $(document).on("click", ".popover button", function (e) {
+                                $(document).on('click', '.popover button', function (e) {
                                     $.post('/tours/log', { tourId: that.tourId, step: 3 });
 
-                                    $(document).off("click", ".popover button");
+                                    $(document).off('click', '.popover button');
 
                                     $('.popover').hide();
 
@@ -48,7 +48,7 @@
                             // if the game's over, show the next step
                             } else if(status === 'done') {
 
-                                $(document).off("click", ".popover button");
+                                $(document).off('click', '.popover button');
 
                                 $('.popover').hide();
 
@@ -66,7 +66,7 @@
 
                                 $.post('/tours/log', { tourId: this.tourId, step: 3 });
 
-                                $(document).off("click", ".popover button");
+                                $(document).off('click', '.popover button');
                             
                                 $('.popover').hide();
 
@@ -84,9 +84,9 @@
                                 trigger: 'manual'
                             });
             
-                            $(document).on("click", ".popover button", function(e) {
+                            $(document).on('click', '.popover button', function(e) {
                                 $.post('/tours/log', { tourId: that.tourId, step: 1 });
-                                $(document).off("click", ".popover button");
+                                $(document).off('click', '.popover button');
 
                                 $elmt.popover('hide');
                                 that.showLogin();
@@ -109,11 +109,11 @@
                                     trigger: 'manual'
                                 });
 
-                            $(document).on("click", ".popover button", function (e) {
+                            $(document).on('click', '.popover button', function (e) {
                                 $.post('/tours/log', { tourId: that.tourId, step: 2 });
                                 $elmt.text('you@example.com').unbind();
 
-                                $(document).off("click", ".popover button");
+                                $(document).off('click', '.popover button');
 
                                 $elmt.popover('hide');
                                 that.showScoring();                                
@@ -138,6 +138,14 @@
                             });                            
 
                             $elmt.popover('show');
+                            setTimeout(function() {
+                                $('.nophone').css('visibility', 'visible');
+                                $(document).on('click', '.nophone button', function(e) {
+                                    e.preventDefault();
+                                    window.open('http://tourney.resounding.ca/tours/mobile/?tourId=' + that.tourId, 'phoneAlternative',
+                                        'menubar=yes,location=yes,resizable=yes,scrollbars=yes,status=yes,width=480;height=600');
+                                });
+                            }, 3000);
                         },
                         showGameOver: function() {
                             var that = this,
@@ -150,7 +158,7 @@
                                 trigger: 'manual'
                             });
 
-                            $(document).on("click", ".popover button", function (e) {
+                            $(document).on('click', '.popover button', function (e) {
                                 $.post('/tours/log', { tourId: that.tourId, step: 4 });
 
                                 $elmt.popover('hide');
